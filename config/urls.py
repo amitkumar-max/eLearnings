cc17fb54131a8a550b0db322a894db9d50b6f3ec
amitkumar-max <ak9268509@gmail.com>
2025-09-19 20:55:34 +0530

    Auto update from PowerShell

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,21 @@
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('users/', include('app.users.urls', namespace='users')),  # ✅ users namespace
+#     path('courses/', include('app.courses.urls', namespace='courses')),  # ✅ courses namespace
+# ]
+
+
+
+from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('users/', include('app.users.urls', namespace='users')),  # ✅ users namespace
-    path('courses/', include('app.courses.urls', namespace='courses')),  # ✅ courses namespace
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
+    path("courses/", include("courses.urls")),
+    
+    # 👇 root redirect ("/" -> "/courses/")
+    path("", RedirectView.as_view(url="/courses/", permanent=False)),
 ]
