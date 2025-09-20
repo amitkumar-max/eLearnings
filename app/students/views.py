9ebc65cc8ff06a7044abeaaddeb11ee0065d51a2
amitkumar-max <ak9268509@gmail.com>
2025-09-20 12:44:39 +0530

    Auto update from PowerShell

--- app/students/views.py
+++ app/students/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def student_dashboard(request):
     # yahan student specific data fetch kar sakte ho
-    return render(request, "students/dashboard.html", {
+    return render(request, "students/templates/students/dashboard.html", {
         "user": request.user
     })
